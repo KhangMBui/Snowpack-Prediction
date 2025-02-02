b8af701988e04c46f053c63aa601d7b054880664
pmuyd <uy.pham@wsu.edu>
2025-02-01 16:37:44 -0800

    create LSTM short object

--- lstm_training.py
+++ lstm_training.py
@@ -2,9 +2,26 @@ import pandas as pd
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
+#define the LTSM model
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out  
 
 def main():
     
@@ -25,6 +42,9 @@ def main():
     #Convert the data into tensors
     X_train = torch.tensor(X_train, dtype=torch.float32)
     X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train.values, dtype=torch.float32).view(-1, 1)
+    y_test = torch.tensor(y_test.values, dtype=torch.float32).view(-1, 1)
+    
 
 if __name__ == '__main__':
     main()
